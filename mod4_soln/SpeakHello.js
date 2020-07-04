99d7276b33cf86c32753e4c37098fb6138f4f71b
srishcodes <63044722+srishcodes@users.noreply.github.com>
2020-07-04 18:48:43 +0530

    Update SpeakHello.js

--- mod4_soln/SpeakHello.js
+++ mod4_soln/SpeakHello.js
@@ -1,7 +1,6 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
-
-
+( function()  {
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
@@ -16,6 +15,7 @@ var speakWord = "Hello";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
+})();
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
 // 'helloSpeaker' on the global scope as well.
