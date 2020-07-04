cea32551b479decf6e27fd7d3477134a7531affd
srishcodes <63044722+srishcodes@users.noreply.github.com>
2020-07-04 19:38:00 +0530

    Update SpeakHello.js

--- mod4_soln/SpeakHello.js
+++ mod4_soln/SpeakHello.js
@@ -4,7 +4,7 @@
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = new Object():
+var helloSpeaker = new Object();
  
   
 
